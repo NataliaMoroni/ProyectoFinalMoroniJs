a49a430d1c0de61f8680ee68b1fbf0d3dea11543
Natalia Moroni <natymoroni22@gmail.com>
2022-08-27 14:13:54 -0300

    guardo trabajo realizado

---
+++ script.js
@@ -0,0 +1,52 @@
+//Mi proyecto será sobre una pagina de nutrición con venta de productos nutricionales o viandas
+
+function saludar (){
+    let nombre = prompt("Ingrese su Nombre");
+    
+     while (nombre === ""){
+        alert ("Por favor ingrese los datos soliitados");
+        nombre = prompt ("Ingrese su Nombre"); 
+    }
+    
+      let mensaje = `Hola ${nombre} . Bienvenid@!!!`;
+      alert (mensaje);
+ }
+ 
+ saludar ();
+ 
+ function imc (){
+ 
+ alert( "Por favor siga las intrucciones para poder obtener correctamente su IMC (indice de masa corporal)");
+ 
+ let estatura = prompt ("Ingrese su Estatura en metros. Debe utilizar punto en lugar de coma");
+ let peso = prompt ("Ingrese su Peso en kg");
+ let resultado = peso /(estatura * estatura);
+ 
+ alert ("Su indice de masa corporal es :" + resultado);
+ 
+ 
+ if (resultado <= 18.5) {
+     alert ("Usted posee Bajo peso");
+ }
+ else if (resultado <= 24.9) {
+     alert ("Su peso peso es normal o adecuado");
+ } 
+ else if(resultado <= 29.9) {
+     alert ("Usted se encuentre en sobrepeso");
+ } 
+ else if (resultado <= 34.9) {
+     alert ("Usted posee obesidad de grado 1");
+ }
+ else if (resultado <= 39.9) {
+     alert ("Usted posee obesidad de grado 2");
+ }
+ else if (resultado >= 40.0) {
+     alert ("Usted posee obesidad grado 3 o morbida");
+ }
+ 
+ alert ("Gracias por su consulta");
+ 
+ }
+ 
+ imc ();
+ 
